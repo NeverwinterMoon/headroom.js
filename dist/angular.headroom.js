1ef9b5e1a2649240be49bd212ad381b7b40e13a7
NeverwinterMoon <manni.calavera@gmail.com>
2015-10-20 18:51:11 +0300

    Build without release version

--- dist/angular.headroom.js
+++ dist/angular.headroom.js
@@ -1,12 +1,12 @@
 /*!
  * headroom.js v0.7.0 - Give your page some headroom. Hide your header until you need it
- * Copyright (c) 2014 Nick Williams - http://wicky.nillia.ms/headroom.js
+ * Copyright (c) 2015 Nick Williams - http://wicky.nillia.ms/headroom.js
  * License: MIT
  */
 
 (function(angular) {
 
-  if(!angular) {
+  if (!angular) {
     return;
   }
 
@@ -17,24 +17,33 @@
   angular.module('headroom', []).directive('headroom', function() {
     return {
       restrict: 'EA',
-      scope: {
+      scope:    {
         tolerance: '=',
-        offset: '=',
-        classes: '=',
-        scroller: '@'
+        offset:    '=',
+        classes:   '=',
+        scroller:  '@',
+        active:    '='
       },
-      link: function(scope, element) {
-        var options = {};
-        angular.forEach(Headroom.options, function(value, key) {
-          options[key] = scope[key] || Headroom.options[key];
+      link:     function(scope, element) {
+        var headroom;
+
+        scope.$watch('active', function(value) {
+          if (!value && headroom) headroom.destroy();
+          if (!value) return;
+
+          var options = {};
+          angular.forEach(Headroom.options, function(value, key) {
+            options[key] = scope[key] || Headroom.options[key];
+          });
+          if (options.scroller) {
+            options.scroller = angular.element(options.scroller)[0];
+          }
+          headroom = new Headroom(element[0], options);
+          headroom.init();
         });
-        if (options.scroller) {
-          options.scroller = angular.element(options.scroller)[0];
-        }
-        var headroom = new Headroom(element[0], options);
-        headroom.init();
+
         scope.$on('$destroy', function() {
-          headroom.destroy();
+          if (headroom) headroom.destroy();
         });
       }
     };
